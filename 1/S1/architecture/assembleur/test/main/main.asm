667677eb071e005b06c3277172f78500c84a1c7a
HAMROUNI YASSIN <yassine.hamrouni@heckel02.u-pem.fr>
2023-10-04 13:37:17 +0200

    4

--- 1/S1/architecture/assembleur/test/main/main.asm
+++ 1/S1/architecture/assembleur/test/main/main.asm
@@ -1,8 +1,8 @@
 section .data
     a dd 10
     b dd 15
-    msg1 db 'ECX superieur a EBX'
-    msg2 db 'ECX inferieur a EBX'
+    msg1 db 'ECX superieur a EBX', 0
+    msg2 db 'ECX inferieur a EBX', 0
 
 section .text
     global _start
@@ -18,12 +18,12 @@ _start:
     mov eax, 4
     mov ebx, 1
     mov ecx, msg1
-    mov edx, 20
+    mov edx, 22
     int 0x80
 
     end_if:
         mov eax, 4
         mov ebx, 1
         mov ecx, msg2
-        mov edx, 20
+	mov edx, 22
         int 0x80
