362165df1aca3dbae23d92c968981bfb78bb76a9
YAS <yassine.uchiwa.91@gmail.com>
2024-01-17 12:29:22 +0100

    a

--- 1/S1/C/tp/tp8/tp8.c
+++ 1/S1/C/tp/tp8/tp8.c
@@ -1,69 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #define SIZE 2000000
 
-
-
-
-int syracuse(long unsigned int nb, int vol, long unsigned int* tab){
-    // printf("%d ", nb);
-    long unsigned int next_nb=0;
-    if(nb == 1){
+int syracuse(long unsigned int nb, int vol, long unsigned int* tab) {
+    if (nb == 1) {
         return vol;
     }
-    else if (nb%2 == 0){ 
-        if(tab[nb] == -1){
-            tab[nb]=nb/2;
-            printf("calculé: %ld ", tab[nb]);
-            next_nb=tab[nb];
-        }
-        else{
-            next_nb=tab[nb];
-            printf("non calculé: %ld ", next_nb);
-        }
-        return syracuse(next_nb, vol+1, tab);
-    }
-    else{
-        if(tab[nb] == -1){
-            tab[nb]=3*nb+1  ;
-            printf("calculé: %ld ", tab[nb]);
-            next_nb=tab[nb];
-        }
-        else{
-            next_nb=tab[nb];
-            printf("non calculé: %ld ", next_nb);
+
+    long unsigned int next_nb = 0;
+
+    if (nb < SIZE && tab[nb] != -1) {
+        // La valeur est déjà calculée, éviter de recalculer
+        next_nb = tab[nb];
+        // printf("non calculé: %ld ", next_nb);
+        printf("%ld ", next_nb);
+    } else {
+        if (nb % 2 == 0) {
+            next_nb = nb / 2;
+        } else {
+            next_nb = 3 * nb + 1;
         }
 
-        return syracuse(tab[nb], vol+1, tab);
+        if (nb < SIZE) {
+            // Mettre en cache la valeur calculée
+            tab[nb] = next_nb;
+            // printf("calculé: %ld ", tab[nb]);
+             printf("%ld ", next_nb);
+        }
     }
+
+    return syracuse(next_nb, vol + 1, tab);
 }
 
-
-int main(int argc, char **argv){
-    long unsigned int* tab= malloc(sizeof(long unsigned int)*SIZE);
-
-    for(int i=0; i<SIZE; i++){
-        tab[i]=-1;
-    }   
-    
-    if(argc<2){
-        fprintf(stderr, "Veuillez utilise ce format: %s numéro\n", argv[0]);
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        fprintf(stderr, "Veuillez utiliser ce format: %s numéro\n", argv[0]);
+        return EXIT_FAILURE;
     }
 
-    int nb=atoi(argv[1]);
+    long unsigned int* tab = malloc(sizeof(long unsigned int) * SIZE);
+    if (!tab) {
+        fprintf(stderr, "Erreur d'allocation mémoire.\n");
+        return EXIT_FAILURE;
+    }
+
+    for (int i = 0; i < SIZE; i++) {
+        tab[i] = -1;
+    }
+
+    int nb = atoi(argv[1]);
     printf("\n%d ", nb);
-    
-    int nb_vol=syracuse(nb, 0, tab);
 
-    printf("\nfly length: %d\n", nb_vol);
+    int nb_vol = syracuse(nb, 0, tab);
 
-    int nb_vol2=syracuse(27, 0, tab);
+    printf("\nLongueur de vol : %d\n", nb_vol);
 
-    printf("\nfly length: %d\n", nb_vol2);
-
-
+    int nb_vol2 = syracuse(53, 0, tab);
+    printf("\nLongueur de vol : %d\n", nb_vol2);
 
     free(tab);
 
-
+    return EXIT_SUCCESS;
 }
