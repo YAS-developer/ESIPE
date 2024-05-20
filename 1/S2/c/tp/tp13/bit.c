641b3d17109c9a1f8e498c504d4d060c6e3db044
YAS <yas@Ubuntu.myguest.virtualbox.org>
2024-05-20 14:20:00 +0200

    l'ecole...

--- 1/S2/c/tp/tp13/bit.c
+++ 1/S2/c/tp/tp13/bit.c
@@ -1,36 +1,17 @@
+#include <limits.h>
+#include <stdio.h>
 #include "bit.h"
-#include <stdlib.h>
 
-
-unsigned long int tab[SIZE][SIZE];
-
-int bit_value_ULI(unsigned long int n, int position){
-    /*
-    int i=SIZE-1, j=SIZE-1;
-    for(; i>=0; i--){
-        for(; j>=0; j--){
-             if(tab[i][j] == n){
-                 return 1;
-        }
-    } 
-    */
-    if(position < 0 || position > 63){
-        fprintf(stderr, "Veuillez renseigner une position entre 0 et 63.");
-        return EXIT_FAILURE;
+void print_ULI(unsigned long int n) {
+    int i;
+    for (i = CHAR_BIT * 8 - 1; i >= 0; i--) {
+        printf("%d", bit_value_ULI(n, i));
     }
-
-    return (int) (n >> position) & 1;
-    
+    printf("\n");
 }
 
-
-void print_ULI(unsigned long int n){
-    int i;
-    printf("%ld en binaire : ", n);
-    for (i = 0; i < 64; i++)
-        printf("%ld", (n >> i ) & 1);
-
-    printf("\n"); 
+int bit_value_ULI(unsigned long int n, int position) {
+    return (int) (n >> position) & 1;
 }
 
 void set_positive_bit_ULI(unsigned long int *n, int position) {
--- 1/S2/c/tp/tp13/main.c
+++ 1/S2/c/tp/tp13/main.c
@@ -1,17 +1,37 @@
 #include <stdio.h>
-#include "bit.h"
+#include "dames.h"
+#include "graphic.h"
 
+#define SIZE 640
 
+int main() {
+    unsigned long int n = 0;
+    int queens[8];
+    int queen_size = 0, mouse_x, mouse_y;
 
+    MLV_create_window("Damier", "aled", SIZE, SIZE);
 
-int main(int argc, char ** argv){
-    
-    unsigned long int nb = 1;
+    draw_board(n, queens, queen_size, SIZE / 8);
 
-    /* printf("%d\n", bit_value_ULI(nb, 1));*/
-    
-    set_positive_bit_ULI(&nb, 1);
-    print_ULI(nb);
+    while (!is_bord_full(n) && queen_size <= 7) {
+        MLV_wait_mouse(&mouse_x, &mouse_y);
+
+        if (from_coordinates_to_cell_index(&mouse_y, &mouse_x)) {
+            if (is_queen_placeable(n, mouse_y, mouse_x)) {
+                set_cells_queen(&n, queens, &queen_size, mouse_y, mouse_x);
+            }
+        }
+        draw_board(n, queens, queen_size, 640 / 8);
+        print_bord_command_line(n);
+    }
+
+    if (queen_size <= 7) {
+        printf("%s", "You lose :(\n");
+    } else {
+        printf("%s", "YOU WIN !!!\n");
+    }
+
+    MLV_free_window();
 
     return 0;
 }
