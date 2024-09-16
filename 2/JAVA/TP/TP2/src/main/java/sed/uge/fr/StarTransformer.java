c093998cc1458214a981d9f0ccbf542adc4a93fb
yas <yassine.uchiwa.91@gmail.com>
2024-09-16 21:48:20 +0200

    TP2 q1 done

--- 2/JAVA/TP/TP2/src/main/java/sed/uge/fr/StarTransformer.java
+++ 2/JAVA/TP/TP2/src/main/java/sed/uge/fr/StarTransformer.java
@@ -2,9 +2,10 @@ package sed.uge.fr;
 
 record StarTransformer(int res) implements Transformer {
 	@Override
-	public String transform(String line) {
-		var tmp = "*".repeat(res);
-		line.replace("*", tmp);
-		return line;
-	}
+    public String transform(String line) {
+        if (res == 0) {
+            return line.replace("*", "");
+        }
+        return line.replace("*", "*".repeat(res));
+    }
 }
--- 2/JAVA/TP/TP2/src/main/java/sed/uge/fr/StreamEditor.java
+++ 2/JAVA/TP/TP2/src/main/java/sed/uge/fr/StreamEditor.java
@@ -5,8 +5,11 @@ import java.io.IOException;
 import java.io.Writer;
 import java.util.Objects;
 
-public record StreamEditor() {
+public final class StreamEditor{
 	
+	private StreamEditor() {
+        throw new AssertionError("This class is not meant to be instantiated");
+    }
 	
 	
 	public static Transformer createTransformer(String s) {
@@ -16,8 +19,8 @@ public record StreamEditor() {
 		return switch(s) {
 	        case "u" -> new UpperCaseTransformer();
 	        case "l" -> new LowerCaseTransformer();
-	        case "*1", "*2", "*3", "*4", "*5", "*6", "*7", "*8", "*9" -> {
-	            int res = Integer.parseInt(s, s.charAt(1));
+	        case String str when str.matches("\\*[0-9]") -> {
+	            int res = Integer.parseInt(s.substring(1));
 	            yield new StarTransformer(res);
 	        }
 	        default -> throw new IllegalArgumentException("invalid argument " + s);
@@ -26,12 +29,14 @@ public record StreamEditor() {
 	
 	
 	public static void rewrite(BufferedReader reader, Writer writer, Transformer transformer) throws IOException {
-		var tmp = new String();
-		var result = new String();
-		while((tmp = reader.readLine()) != null) {
-			result = transformer.transform(tmp);
-			writer.write(result+"\n");
-		}
-	}
+        Objects.requireNonNull(reader, "reader must not be null");
+        Objects.requireNonNull(writer, "writer must not be null");
+        Objects.requireNonNull(transformer, "transformer must not be null");
+        
+        var line = new String();
+        while ((line = reader.readLine()) != null) {
+            writer.write(transformer.transform(line) + "\n");
+        }
+    }
 
 }
