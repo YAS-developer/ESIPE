5a2775f3cbd6a4b980830bc70ced6db265a7eec8
yassine.hamrouni <yassine.uchiwa.91@gmail.com>
2025-03-20 10:31:59 +0100

    ex2

---
+++ 2/S2/progreseau/tp2/src/fr/uge/net/udp/ex1/NetcatUDP.java
@@ -0,0 +1,45 @@
+package fr.uge.net.udp.ex1;
+
+import java.io.IOException;
+import java.nio.channels.DatagramChannel;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.util.Scanner;
+
+public class NetcatUDP {
+    public static final int BUFFER_SIZE = 1024;
+
+    private static void usage() {
+        System.out.println("Usage : NetcatUDP host port charset");
+    }
+
+    public static void main(String[] args) throws IOException {
+        if (args.length != 3) {
+            usage();
+            return;
+        }
+
+        var server = new InetSocketAddress(args[0], Integer.parseInt(args[1]));
+        var cs = Charset.forName(args[2]);
+        var buffer = ByteBuffer.allocate(BUFFER_SIZE);
+        try( var dc = DatagramChannel.open() ){
+        	dc.bind(null);
+        	 try (var scanner = new Scanner(System.in);) {
+                 while (scanner.hasNextLine()) {
+                     var line = scanner.nextLine();
+                     buffer.put(line.getBytes());
+                     buffer.flip();
+                     dc.send(buffer, server);
+                     buffer.clear();
+                     dc.receive(buffer);
+                     buffer.flip();
+                     var cb = cs.decode(buffer);
+                     System.out.println(cb.toString());
+                     buffer.clear();
+                 }
+                 
+             }
+        }
+    }
+}
