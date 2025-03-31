e7e4e220b2ecb4a9c2965324d614ee6483daff7f
yassine.hamrouni <yassine.uchiwa.91@gmail.com>
2025-03-31 10:00:25 +0200

    concu rev

--- 2/RATTRAPAGES/CONCU/EXAM2023/src/secretsanta/SecretSanta.java
+++ 2/RATTRAPAGES/CONCU/EXAM2023/src/secretsanta/SecretSanta.java
@@ -11,10 +11,12 @@ class SecretSanta{
 	private final List<Integer> order;
 	private final ReentrantLock lock = new ReentrantLock();
 	private final Condition condition1 = lock.newCondition();
+	private final Condition condition2 = lock.newCondition();
 	private final String[] hat;
 	private int count;
+	private int count2;
 	
-	public SecretSanta(int nbParticipants, List<Integer> order ) {
+	public SecretSanta(int nbParticipants, List<Integer> order) {
 		Objects.requireNonNull(order);
 		if(nbParticipants <= 0) {
 			throw new IllegalArgumentException();
@@ -56,9 +58,30 @@ class SecretSanta{
 	}
 	
 	
+	public List<String> observe(int n){
+		
+		lock.lock();
+		try {
+			while(count < n && !Thread.currentThread().isInterrupted()) {	
+				condition2.await();
+			}
+			condition2.signalAll();
+		}
+		catch (InterruptedException e){
+			
+		}
+		finally {
+			lock.unlock();
+		}
+		
+		return null;
+	}
+	
+	
 	
 	
 	public static void main(String[] args) {
+//		EX1 Q1
         var secretSanta = new SecretSanta(5, List.of(0, 1, 3, 4, 2));
         int[] waitingTimes = {500, 1000, 1500, 2000, 2500};
         for (var id = 0; id < waitingTimes.length; id++) {
@@ -67,12 +90,29 @@ class SecretSanta{
                 try {
                     Thread.sleep(waitingTime);
                     var currentThreadName = Thread.currentThread().getName();
-                    System.out.println(currentThreadName + " received " + secretSanta.submit(currentThreadName));
+                   System.out.println(currentThreadName + " received " + secretSanta.submit(currentThreadName));
                 } catch (InterruptedException e) {
-                    throw new AssertionError(e);
+                    throw new AssertionError(e);                }
+            });
+        }
+		
+		
+        int[] observedSizes = {1,2,3,4,5};
+        var observers = new Thread[observedSizes.length];
+        for (var id = 0; id < observedSizes.length; id++) {
+            var observedSize = observedSizes[id];
+            observers[id]=Thread.ofPlatform().name("Observer " + id).start(() -> {
+                var observerName = Thread.currentThread().getName();
+                try {
+                    System.out.println(observerName + " observed " + secretSanta.observe(observedSize));
+                } catch (InterruptedException e) {
+                    System.out.println(observerName + " did not observe any thing as a InterruptedException was thrown");
                 }
             });
         }
+        Thread.sleep(1100);
+        observers[2].interrupt();  
+		
 		
     } 
 }
--- 2/RATTRAPAGES/CONCU/EXAM2023/src/secretsanta/Toto.java
+++
@@ -1,7 +0,0 @@
-package secretsanta;
-
-public class Toto {
-	public static void main(String[] args) {
-		System.out.println("HELLO");
-	}
-}
